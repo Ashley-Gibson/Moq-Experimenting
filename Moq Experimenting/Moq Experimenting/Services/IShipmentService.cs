d676ce45119dd421ae52eaedab0c1847f5da24cf
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-10-21 16:49:35 +0100

    task/init-solution - Added all base code for the solution before using Moq

---
+++ Moq Experimenting/Moq Experimenting/Services/IShipmentService.cs
@@ -0,0 +1,10 @@
+﻿using Moq_Experimenting.Services.Models;
+using System.Collections.Generic;
+
+namespace Moq_Experimenting.Services
+{
+    public interface IShipmentService
+    {
+        void Ship(IAddressInfo info, IEnumerable<ICartItem> items);
+    }
+}
